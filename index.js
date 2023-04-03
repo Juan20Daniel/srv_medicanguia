1d8a33dc8ca5f397fbcfddecf9eddc7da1ab42cb
El cacas <juandaniel200031@gmail.com>
2023-04-02 22:14:04 -0600

    new send

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'images')));
